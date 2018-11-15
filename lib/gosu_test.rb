86c0f49fc297d32221f9feb16ce41b975a87f408
itggot-jacob-sandstrom <jacob.sandstrom@elev.ga.ntig.se>
2018-11-15 10:13:45 +0100

    added caption

--- lib/gosu_test.rb
+++ lib/gosu_test.rb
@@ -1,11 +1,14 @@
 require 'gosu'
 
-class Game < Gosu::Window
+class Gosu_test < Gosu::Window
     def initialize
-        @width = 800
-        @height = 600
-        super @width, @height, false
+        width = 800
+        height = 600
+        super width, height
+        self.caption = "Gosu test"
     end
+
+    
 end
 
-Game.new.show
+Gosu_test.new.show
