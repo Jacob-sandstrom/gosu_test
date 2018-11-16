4d195693944dda7c1a49c34277933f02d54acc09
itggot-jacob-sandstrom <jacob.sandstrom@elev.ga.ntig.se>
2018-11-17 00:27:22 +0100

    made something

--- lib/gosu_test.rb
+++ lib/gosu_test.rb
@@ -3,42 +3,59 @@ require 'gosu'
 
 class Player
     def initialize
-        @image = Gosu::Image.new("../img/shipTransparent.png")
-        @x = @y = @vel_x = @vel_y = @angle = 0.0
-        @score = 0
+        @image = Gosu::Image.new("../img/CharacterSquare.png")
+        
+        @direction = 0
+        @speed = 3
+        
+        @grav = 1
+        @default_jump_speed = 20
+        @jump_speed = @default_jump_speed
+        @in_air = false
+        @x = @y = 0.0
+        
 
     end
   
     def warp(x, y)
       @x, @y = x, y
     end
-    
-    def turn_left
-      @angle -= 4.5
+
+    def go_left
+      @direction = -1
     end
     
-    def turn_right
-      @angle += 4.5
+    def go_right
+      @direction = 1
     end
-    
-    def accelerate
-      @vel_x += Gosu.offset_x(@angle, 0.5)
-      @vel_y += Gosu.offset_y(@angle, 0.5)
+
+    def dont_move
+        @direction = 0
+    end
+
+    def jump
+      @in_air = true
+      @jump_speed = @default_jump_speed
+
     end
     
     def move
-      @x += @vel_x
-      @y += @vel_y
+      @x += @direction * @speed
       @x %= 800
-      @y %= 600
+    #   @y %= 600
       
-      @vel_x *= 0.95
-      @vel_y *= 0.95
+      if @in_air
+        @y -= @jump_speed    
+        @jump_speed -= @grav
+      end
+
+
     end
   
     def draw
-      @image.draw_rot(@x, @y, 1, @angle)
+      @image.draw(@x, @y, 1)
     end
+
   end
 
 
@@ -50,21 +67,45 @@ class Gosu_test < Gosu::Window
         super width, height
         self.caption = "Gosu test"
 
+        
+        @height_minus_char_height = 568
+
         @player = Player.new
+        @player.warp(400, 568)
 
     end
 
     def update
-        if Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT
-            @player.turn_left
-          end
-          if Gosu.button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT
-            @player.turn_right
-          end
-          if Gosu.button_down? Gosu::KB_UP or Gosu::button_down? Gosu::GP_BUTTON_0
-            @player.accelerate
-          end
-          @player.move
+        #
+        #   Movement
+        #
+        if (Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT) and (Gosu.button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT)
+            @player.dont_move
+        elsif Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT
+            @player.go_left
+        
+        elsif Gosu.button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT
+            @player.go_right
+        elsif  !(Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT) and !(Gosu.button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT)
+            @player.dont_move
+        end 
+        
+        #
+        #   Jumping
+        #
+        if (Gosu.button_down? Gosu::KB_UP or Gosu::button_down? Gosu::GP_BUTTON_0) and !@in_air
+            @player.jump
+        end
+
+        # if @y >= @height_minus_char_height
+        #     @in_air = false
+        #     @jump_speed = 0
+        #     @y = @height_minus_char_height
+
+        # end
+        
+
+            @player.move
     end
 
     def draw
