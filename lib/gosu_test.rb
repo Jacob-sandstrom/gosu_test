6653c91a2cd7c24b7baee759fbfdf1a6b4eca650
itggot-jacob-sandstrom <jacob.sandstrom@elev.ga.ntig.se>
2018-11-16 18:20:54 +0100

    image

--- lib/gosu_test.rb
+++ lib/gosu_test.rb
@@ -6,6 +6,17 @@ class Gosu_test < Gosu::Window
         height = 600
         super width, height
         self.caption = "Gosu test"
+
+        @background_image = Gosu::Image.new("Lennart2.png", :tilable => true)
+
+
+
+    end
+
+    def draw
+
+        @background_image.draw(0,0,0)
+
     end
 
     
